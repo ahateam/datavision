8a631c7829aff886f46d50a1f9b9a7fd8b7edd5f
admin@3ch.org.cn <admin@3ch.org.cn>
2019-07-28 16:43:41 +0800

    增加logback配置文件

--- src/main/java/xhj/cn/start/DVVerticle.java
+++ src/main/java/xhj/cn/start/DVVerticle.java
@@ -2,7 +2,7 @@ package xhj.cn.start;
 
 import io.vertx.core.Vertx;
 import zyxhj.core.controller.TestController;
-import zyxhj.flow.controller.FlowController;
+import zyxhj.flow.service.FlowService;
 import zyxhj.utils.Singleton;
 import zyxhj.utils.ZeroVerticle;
 
@@ -25,7 +25,7 @@ public class DVVerticle extends ZeroVerticle {
 
 		initCtrl(ctrlMap, Singleton.ins(TestController.class, "test"));
 
-		initCtrl(ctrlMap, Singleton.ins(FlowController.class, "flow"));
+		initCtrl(ctrlMap, Singleton.ins(FlowService.class, "flow"));
 
 	}
 
